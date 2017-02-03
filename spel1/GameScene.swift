438872a73f9c39a2dd00fe44736e284983ef13d0
Oliver Buvik <oliverbuvik@Olivers-MacBook-Pro.local>
2017-02-03 12:45:18 +0100

    bg

--- spel1/GameScene.swift
+++ spel1/GameScene.swift
@@ -9,21 +9,45 @@
 import SpriteKit
 import GameplayKit
 
+
+
 class GameScene: SKScene {
+    var bg = SKSpriteNode()
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var didLoad = false;
   
     
     func setUpGame (){
         
+        //Bakgrunden : 600 × 385
+        let bgTexture = SKTexture(imageNamed: "bakgrund.png")
+        let moveBGAnimation = SKAction.move(by: CGVector(dx: -bgTexture.size().width, dy: 0), duration: 10)
+        let shiftBGAnimation = SKAction.move(by: CGVector(dx: bgTexture.size().width, dy:0), duration: 0)
+        let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
+        var i:CGFloat = 0
+        
+        while i < 3 {
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width * i, y: self.frame.midY)
+            bg.size.height = self.frame.height
+            bg.size.width = (self.frame.height / 385) * 600;
+            bg.run(moveBGForever)
+            self.addChild(bg)
+            i += 1
+        }
     }
     
     override func didMove(to view: SKView) {
+        if(!didLoad){
+            didLoad = true;
+            start();
         }
+    }
         
     
-    
+    func start(){
+        setUpGame();
+    }
     
     
     func touchDown(atPoint pos : CGPoint) {
